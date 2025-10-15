e276fe6e519174803005ed34ca8498f43a8bf491
neurobloomai <amarnath@neurobloom.ai>
2025-10-15 14:48:00 -0400

    Update __init__.py

--- pact_ax/primitives/__init__.py
+++ pact_ax/primitives/__init__.py
@@ -5,13 +5,10 @@ Core collaboration primitives for multi-agent systems.
 """
 
 from .story_keeper import StoryKeeper, StoryArc
-
-# Other primitives when ready
-# from .context_share import ContextShareManager
-# from .state_transfer import StateTransferManager
-# from .policy_align import PolicyAlignmentManager
+from .state_transfer import StateTransferManager
 
 __all__ = [
     "StoryKeeper",
     "StoryArc",
+    "StateTransferManager",
 ]

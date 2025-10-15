bcc1959186bae78676b3725ec81c00a57410b3e5
neurobloomai <amarnath@neurobloom.ai>
2025-10-15 14:18:42 -0400

    Update base_primitive.py

---
+++ pact_ax/core/base_primitive.py
@@ -0,0 +1,13 @@
+"""
+Base primitive classes for PACT-AX
+(Stub for now - to be implemented)
+"""
+
+class PACTConfig:
+    """Configuration for PACT primitives"""
+    pass
+
+
+class PACTPrimitive:
+    """Base class for PACT primitives"""
+    pass
